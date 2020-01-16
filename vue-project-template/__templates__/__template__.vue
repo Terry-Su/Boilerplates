a5c009ad0bc3910ccece0d3b675c597cc205a51a
Terry Su <theterrysu@163.com>
2020-01-16 23:49:16 +0800

    adding vue project template

---
+++ vue-project-template/__templates__/__template__.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Template</div>
+</template>
+<script>
+export default {
+  name: '',
+  props: {},
+  data () { return {} },
+  computed: {},
+  watch: {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style scoped>
+
+</style>
---
+++ vue-project-template/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    App
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'App',
+  props: {},
+  data () { return {} },
+  computed: {},
+  watch: {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style scoped>
+div {
+  color: deepskyblue;
+}
+</style>
