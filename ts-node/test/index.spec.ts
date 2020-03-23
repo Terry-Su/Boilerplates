745db48f5c2ae809aeba56f5a6daaefbfe27f1c0
Terry Su <theterrysu@163.com>
2020-03-23 14:52:16 +0800

    ts-node test: set timeout for async units

--- ts-node/test/index.spec.ts
+++ ts-node/test/index.spec.ts
@@ -1,7 +1,9 @@
 import { hello } from "../src/index"
 const assert = require( 'assert' )
 
-describe( "Test", () => {
+describe( "Test", function() {
+  this.timeout( 20000 )
+
   it( "Sync unit ", () => {
     hello()
   } )
