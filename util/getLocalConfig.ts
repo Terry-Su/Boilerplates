eba6e48345ec23650aa74fb527a6a1437da681d1
Terry Su <theterrysu@163.com>
2020-03-23 14:46:36 +0800

    nodemon: add

--- util/getLocalConfig.ts
+++ util/getLocalConfig.ts
@@ -1,7 +1,7 @@
 import FS from 'fs-extra'
 import PATH from 'path'
 
-export default function getCustomConfig() {
+export default function getLocalConfig() {
   try {
       const customConfigFilePath = PATH.resolve( __dirname, "../../../localConfig.ts" )
       if ( FS.pathExistsSync( customConfigFilePath ) ) {
