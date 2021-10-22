92bcebdf507df61fbe0f07118a24eff2446ffefb
Terry Su <theterrysu@163.com>
2021-10-22 16:14:56 +0800

    update

--- ts-projects/webpack-react-typescript-tredux/src/models/index.ts
+++
@@ -1,9 +0,0 @@
-const files = require.context('.', true, /\.ts$/)
-
-const modules = []
-files.keys().forEach( (key) => {
- if ( key === './index.ts' ) return
- modules.push(files( key ).default)
-} )
-
-export default modules
