eea535b669129c6cb4e3ed9a6d1b3351ae8c0c66
Terry Su <theterrysu@163.com>
2021-12-21 22:17:30 +0800

    update

---
+++ snippets/curl.bash
@@ -0,0 +1,2 @@
+curl -X POST -H "Content-Type: application/json" -d 'UserName=user1&Pwd=test' http://localhost:8090/Register
+curl -X POST -H "Content-Type: application/json" -d '{UserName: "user1", Pwd: "test"}' http://localhost:8090/Register
