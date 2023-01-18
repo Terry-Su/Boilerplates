05cff98195c6fed6a5ac51549d3698fed92885ab
Terry Su 苏溪云 <2684759855@qq.com>
2023-01-18 17:01:30 +0800

    update

--- ts-projects/webpack-react-typescript-tredux/src/__templates__/__TemplateModel__.ts
+++ ts-projects/webpack-react-typescript-tredux/src/__templates__/__TemplateModel__.ts
@@ -1,16 +1,16 @@
 import { getModelHelpers } from "@tredux/tredux"
 
-const M__Templae__ = '__Template__'
-const helper = getModelHelpers<__Templae__State, __Templae__Methods>(M__Templae__)
-export const { useModelState: ums__Templae__, updateModel: um__Templae__, dispatchModel: dm__Templae__, getModelState: gms__Templae__ } = helper
+const M__Template__ = '__Template__'
+const helper = getModelHelpers<__Template__State, __Template__Methods>(M__Template__)
+export const { useModelState: ums__Template__, updateModel: um__Template__, dispatchModel: dm__Template__, getModelState: gms__Template__ } = helper
 const { updateModel: update,  dispatchModel: dispatch, getModelState: getState } = helper
 
 
-class __Templae__State {
+class __Template__State {
 }
 
 
-class __Templae__Methods {
+class __Template__Methods {
   test = async (_, first: string) => {
     console.log( _, first )
   }
@@ -18,7 +18,7 @@ class __Templae__Methods {
 
 
 export default {
-  name: M__Templae__,
-  state: new __Templae__State(),
-  methods: new __Templae__Methods()
+  name: M__Template__,
+  state: new __Template__State(),
+  methods: new __Template__Methods()
 }
--- vue-project-template/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App'
-
-// eslint-disable-next-line no-new
-new Vue({
-  el: '#app',
-  components: { App },
-  render: h => h(App)
-})
